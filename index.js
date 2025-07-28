bd64415d417e6fe9c94faf78434b8f7f94602426
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-07-28 21:56:41 +0200

    Add initial implementation of language parser with production input handling

---
+++ index.js
@@ -0,0 +1,106 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const productions = []
+const grammar = {}
+
+const isValidProduction = (input) => {
+    return /^[A-Z]:\s*[^|\|\s]+[^|]*(\s*\|\s*[^|\|\s]+[^|]*)*\s*$/.test(input)
+}
+
+const formatProduction = (input) => {
+    return input.replace(/\s/g, '')
+}
+
+const enterProductions = () => {
+    rl.question('Enter a production: ', (input) => {
+        if (!isValidProduction(input)) {
+            console.log(
+                `No valid production found for ${input}. Please try again.`
+            )
+            enterProductions()
+        } else {
+            productions.push(formatProduction(input))
+            enterAdditionalProductions()
+        }
+    })
+}
+
+const enterAdditionalProductions = () => {
+    rl.question('Enter another production or type "done": ', (input) => {
+        if (input.toLowerCase() === 'done') {
+            enterWordToBeParsed()
+        } else if (!isValidProduction(input)) {
+            console.log(
+                `No valid production found for ${input}. Please try again.`
+            )
+            enterAdditionalProductions()
+        } else {
+            productions.push(formatProduction(input))
+            enterAdditionalProductions()
+        }
+    })
+}
+
+const enterWordToBeParsed = () => {
+    rl.question('Please enter the word to be parsed: ', (input) => {
+        splitProductions()
+    })
+}
+
+const productionInstructions = () => {
+    console.log('Please enter your productions in the following format:')
+    console.log('')
+    console.log("S → A'Ab | ε   corresponds to   S: A'Ab | _")
+    console.log('')
+    console.log(
+        'Please type in the productions one after the other and confirm each one with ENTER'
+    )
+}
+
+const removeDirectLeftRecursion = () => {
+    Object.entries(grammar).map(([lhs, rhs]) => {
+        let hasDirectLeftRecursion = false
+        rhs.map((r) => {
+            if (lhs.charAt(0) === r[0]) {
+                hasDirectLeftRecursion = true
+            }
+        })
+        if (hasDirectLeftRecursion) {
+            grammar[`${lhs}'`] = []
+        }
+        console.log(grammar)
+    })
+}
+
+const splitProductions = () => {
+    let splitProductions = []
+
+    productions.map((item) => {
+        const [lhs, rhs] = item.split(':')
+        rhs.split('|').map((i) => {
+            splitProductions.push(JSON.stringify([lhs, i]))
+        })
+    })
+    splitProductions = [...new Set(splitProductions)].map((item) => {
+        return JSON.parse(item)
+    })
+
+    splitProductions.map(([lhs, rhs]) => {
+        const symbols = rhs.split('')
+        if (!grammar[lhs]) grammar[lhs] = []
+        grammar[lhs].push(symbols)
+    })
+    removeDirectLeftRecursion()
+}
+
+const main = () => {
+    productionInstructions()
+    enterProductions()
+}
+
+main()
